c0ff5f4a3f6004ce45f819fa317bbc0a7a09fa45
killian.mauge <killian.mauge@edu.esiee.fr>
2026-09-17 11:34:25 +0200

    flashbacks + enigme done

--- src/systems/game_state.py
+++ src/systems/game_state.py
@@ -16,6 +16,8 @@ DEFAULT_FLAGS = {
     "seen_letters": False,
     "seen_office_intro": False,
     "seen_void_reveal": False,
+    "seen_pantin_note": False,
+    "door_chambre_open": False,
     "tutorial_done": False,
     "death_count": 0,
 }
